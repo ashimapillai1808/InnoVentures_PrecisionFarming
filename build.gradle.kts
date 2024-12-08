ce5887b4a1136d15eac208e5105f40bcbd67e936
Susmita Kumari <147643447+susmitakumari14@users.noreply.github.com>
2024-12-08 14:33:19 +0530

    jerry

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,55 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.paruluniversity.innoventures_precisionfarming"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.paruluniversity.innoventures_precisionfarming"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth) // Ensure this is the latest version
+    testImplementation(libs.junit) // Ensure this is the latest version
+    androidTestImplementation(libs.androidx.junit) // Ensure this is the latest version
+    androidTestImplementation(libs.androidx.espresso.core) // Ensure this is the latest version
 }
