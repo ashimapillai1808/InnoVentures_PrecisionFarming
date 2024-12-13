a8a41299691476233ae05accfcdcef27695173da
[Mohitpatel242] <[mohitdpatel242@gmail.com]>
2024-12-14 00:04:10 +0530

    activity_water_tank_status.xml

---
+++ app/src/main/java/com/paruluniversity/innoventures_precisionfarming/water_tank_status.kt
@@ -0,0 +1,20 @@
+package com.paruluniversity.innoventures_precisionfarming
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class water_tank_status : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_water_tank_status)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
